d86e97533f05ca7a0f12aeaa4d1b08a58041d8c3
Sch3lp <sch3lp@gmail.com>
2014-12-17 00:03:25 +0100

    fix build

--- test/spec/controllers/menu.js
+++ test/spec/controllers/menu.js
@@ -6,17 +6,26 @@ describe('Controller: MenuCtrl', function () {
   beforeEach(module('fiAngulartjeApp'));
 
   var MenuCtrl,
-    scope;
+    scope, httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $injector) {
     scope = $rootScope.$new();
+    httpBackend = $injector.get('$httpBackend');
     MenuCtrl = $controller('MenuCtrl', {
       $scope: scope
     });
+    httpBackend
+      .expect('GET','/api/v1/categories/categories.json')
+      .respond([
+        {'id' : '1', 'name': 'Cheese', 'img': '/images/250px-Sandvich.png'},
+        {'id' : '2', 'name': 'Ham', 'img': '/images/250px-Sandvich.png'}]
+      );
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should get a list of categories to put on the scope', function () {
+    httpBackend.flush();
+    expect(scope.categories.length).toBe(2);
+    expect(scope.categories[0]).toEqual({'id' : '1', 'name': 'Cheese', 'img': '/images/250px-Sandvich.png'});
   });
 });
