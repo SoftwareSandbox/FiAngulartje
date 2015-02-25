2d1611465c588ffd695730fe17a13d85c4b85c0f
Jonathan Vanoverloop <Jooones@users.noreply.github.com>
2015-02-25 19:44:39 +0100

    jonathan & hannes: #3 simplified test setup

--- test/spec/directives/fiproduct.js
+++ test/spec/directives/fiproduct.js
@@ -8,12 +8,10 @@ describe('The fiproduct directive', function () {
   // load the directives template as js via ng-html2js
   beforeEach(module('views/directives/fiproduct.html'));
 
-  var element,
-    $scope,
-    product;
+  var scope;
 
   beforeEach(inject(function ($rootScope, $compile) {
-    product = {
+    var productStub = {
       'id': '1',
       'categoryId': '1',
       'name': 'Club cheese',
@@ -29,18 +27,18 @@ describe('The fiproduct directive', function () {
       'type': 'Club',
       'price': 2.0
     };
-    element = angular.element('<fiproduct product=product></fiproduct>');
-    $compile(element)($rootScope.$new());
-    $rootScope.$apply(function(){
-      $rootScope.product = product;
-    });
-    $rootScope.$digest();
+    var initialScope = $rootScope.$new();
+    initialScope.product = productStub;
 
-    $scope = element.isolateScope();
+    var element = '<fiproduct product=product></fiproduct>';
+    element = $compile(element)(initialScope);
+
+    initialScope.$digest();
+    scope = element.isolateScope();
   }));
 
   it('should print out a sandwiches name, ingredients, sauces and price', inject(function () {
-    expect($scope.ingredients).toBe('gouda cheese, salad, tomatoes, egg, mayonaise');
+    expect(scope.ingredients).toBe('gouda cheese, salad, tomatoes, egg, mayonaise');
   }));
 
 });
