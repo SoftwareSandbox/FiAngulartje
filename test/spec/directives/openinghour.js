8a42bd40730252d8c1518b7dc7f4cea1d1b9fe84
Toon <borgers.toon@gmail.com>
2015-02-26 12:45:39 +0100

    fix test

--- test/spec/directives/openinghour.js
+++ test/spec/directives/openinghour.js
@@ -33,6 +33,19 @@ describe('The openinghour directive', function () {
     scope = element.isolateScope() || element.scope();
   }));
 
+  it('html content gets built correctly', function () {
+    var children = element.find('li');
+    expect(children.length).toBe(1);
+    var data = children.find('data');
+    expect(data.attr('datedata')).toEqual('Mo 09:00-12:00');
+
+    var spans = data.children();
+    expect(spans.length).toBe(2);
+
+    expect(spans[0].innerText).toEqual('Monday');
+    expect(spans[1].innerText).toEqual('09:00-12:00');
+  });
+
   it('translateDay is able to translate all days', function () {
     expect(scope.translateDay(1)).toEqual('Monday');
     expect(scope.translateDay(2)).toEqual('Tuesday');
@@ -45,16 +58,16 @@ describe('The openinghour directive', function () {
 
   it('formatHours works for single entry', function () {
     expect(scope.formatHours([{'from': '09:00', 'until': '12:00'}])).toEqual('09:00-12:00');
-  })
+  });
 
   it('formatHours works for multiple entrie', function () {
     expect(scope.formatHours([{'from': '09:00', 'until': '12:00'}, {'from': '13:00', 'until': '17:00'}]))
       .toEqual('09:00-12:00, 13:00-17:00');
-  })
+  });
 
   it('microdata creates correct microdata string', function () {
     expect(scope.microdata(openinghours[0])).toEqual('Mo 09:00-12:00');
-  })
+  });
 
   it('openinghours should be put on scope', function () {
     expect(scope.openinghours.length).toBe(1);
@@ -62,6 +75,6 @@ describe('The openinghour directive', function () {
     expect(scope.openinghours[0].hours.length).toBe(1);
     expect(scope.openinghours[0].hours[0].from).toEqual('09:00');
     expect(scope.openinghours[0].hours[0].until).toEqual('12:00');
-  })
+  });
 
 });
