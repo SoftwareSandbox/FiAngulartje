19e3b08ef83ab8996b118b62f2cb71f39f3fc5ea
Toon <borgers.toon@gmail.com>
2015-02-26 08:10:04 +0100

    Openinghours service and directive

---
+++ app/scripts/services/Openinghours.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name fiAngulartjeApp.openinghours
+ * @description
+ * # openinghours
+ * Service in the fiAngulartjeApp.
+ */
+
+angular.module('fiAngulartjeApp')
+  .service('Openinghours', ['$resource','Fiazard', function ($resource, Fiazard) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+    return $resource(Fiazard.baseurl + 'api/v1/openinghours/',{});
+  }]);
