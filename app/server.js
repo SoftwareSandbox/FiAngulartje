9c7577f88b3329c1939543c23087fd63e6b45051
mdeswer1 <mike.de.swert@staff.telenet.be>
2015-05-27 15:47:05 +0200

    server.js now works with npm start

--- app/server.js
+++ app/server.js
@@ -5,11 +5,10 @@ var app = express();
 app.context = '/app';
 
 var server = app.listen(9000, function() {
-    var host = server.address().address;
     var port = server.address().port;
 
-    console.log('FiAngulartje server listening at http://%s:%s', host, port);
+    console.log('FiAngulartje server listening on port %s', port);
 });
 
-app.use('/', express.static('./'));
+app.use('/', express.static(__dirname));
 app.use('/bower_components', express.static('./bower_components'));
