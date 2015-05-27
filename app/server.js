1349a3a11968c2d295a059fa3c208f6bb4686639
mdeswer1 <mike.de.swert@staff.telenet.be>
2015-05-27 15:14:31 +0200

    added server.js file to run with node

---
+++ app/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+app.context = '/app';
+
+var server = app.listen(8423, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('FiAngulartje server listening at http://%s:%s', host, port);
+});
+
+app.use('/', express.static('./'));
+app.use('/bower_components', express.static('./bower_components'));
